53d1916a7c26b48197acd40051a0519c0ec3f8c3
Eric Hennenfent <Eric.Hennenfent@nccgroup.trust>
2017-08-14 16:00:51 -0500

    Run

--- debugger.py
+++ debugger.py
@@ -6,6 +6,7 @@ from util import *
 
 llil = None
 emilator = None
+bv = None
 
 is_running = False
 
@@ -28,7 +29,17 @@ def finish(_args):
     pass
 
 def run(_args):
-    pass
+    global emilator, bv
+    ### TODO: Implement platform-dependent loading conventions accurate to the real thing
+    for reg in sorted(bv.arch.full_width_regs):
+        emilator.set_register_value(reg, 0)
+    emilator.set_register_value('rbp', 0xf0000)
+    emilator.set_register_value('rsp', 0xf0000)
+    emilator.map_memory(start=0x0, length=0x100000)
+
+    for i in emilator.run():
+        print('{}:{} completed: {} -- {}'.format(i.function.source_function.name, i.instr_index, i, i.operation.name))
+
 
 def kill(_args):
     pass
@@ -44,8 +55,7 @@ def info(args):
         coverage.test_coverage(emilator, verbose=True)
 
 def load_binary(binary, _args):
-    global emilator
-    global llil
+    global emilator, llil, bv
 
     bv = BinaryViewType.get_view_of_file(binary)
     start = bv.symbols['_start'].address
