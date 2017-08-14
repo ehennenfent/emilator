19202d668a54437789a31e33eaeef0fdd9a558e9
Eric Hennenfent <Eric.Hennenfent@nccgroup.trust>
2017-08-14 14:57:11 -0500

    Super basic debugger boilerplate

---
+++ console.py
@@ -0,0 +1,45 @@
+from prompt_toolkit import prompt
+from prompt_toolkit.contrib.completers import WordCompleter
+from debugger import *
+import argparse
+import traceback
+
+parser = argparse.ArgumentParser(description='Process some integers.')
+parser.add_argument('binary', type=str, help='A binary or .bndb')
+arguments = parser.parse_args()
+
+cmd_map = [
+    (['quit', 'q', 'exit'], exit_wrapper),
+    (['nexti', 'ni'], step_over),
+    (['stepi', 'si'], step_in),
+    (['continue', 'c'], continue_to),
+    (['break', 'b'], breakpoint),
+    (['finish'], finish),
+    (['run', 'r'], run),
+    (['kill', 'ki', 'k'], kill),
+    (['info', 'i',], info),
+]
+
+def dispatch(verb, args):
+    found = False
+    for entry in cmd_map:
+        if verb in entry[0]:
+            found = True
+            try:
+                entry[1](args)
+                break
+            except:
+                traceback.print_exc()
+                break
+    if not found:
+        print("No such command: {}".format(verb))
+
+if __name__ == '__main__':
+    cmd_completer = WordCompleter([cmd for item in cmd_map for cmd in item[0] if len(cmd) > 2])
+    while(True):
+        answer = prompt(u'(emILator) ', completer = cmd_completer)
+        lexed = answer.split(' ')
+        if len(lexed) == 1:
+            dispatch(lexed[0], [])
+        elif len(lexed) > 1:
+            dispatch(lexed[0], lexed[1:])
---
+++ debugger.py
@@ -0,0 +1,28 @@
+from __future__ import print_function
+
+def exit_wrapper(_args):
+    exit(0)
+
+def step_over(args):
+    pass
+
+def step_in(args):
+    pass
+
+def breakpoint(args):
+    pass
+
+def continue_to(args):
+    pass
+
+def finish(_args):
+    pass
+
+def run(_args):
+    pass
+
+def kill(_args):
+    pass
+
+def info(args):
+    pass
